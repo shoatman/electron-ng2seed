b478ebc92ec5b15e8e1dfd22ff38ba75ac7ac210
Shane Oatman <shoatman@microsoft.com>
2016-10-21 16:03:31 -0700

    Updates for authorization service and calling code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,29 @@ import {AuthorizationService} from './authorization.service.ts';
 })
 export class AppComponent { 
 
+	private armResourceId: string = "797f4846-ba00-4fd7-ba43-dac1f8f63013";
+
 	constructor(private authorizationService: AuthorizationService){}
 
 	authorize() {
-		this.authorizationService.signIn();
+		this.authorizationService.signIn(this.userSignedIn);
 	}
+
+	private userSignedIn(err:string, token:any){
+		console.log(token);
+	}
+
+	getCachedUser(){
+		console.log(this.authorizationService.getCachedUser());
+	}
+
+	getARMAccessToken(){
+		this.authorizationService.acquireToken(this.accessTokenCallback, this.armResourceId);
+	}
+	
+	private accessTokenCallback(err:string, token:any){
+		console.log(token);
+	}
+
 
 }
--- src/app/authorization.service.ts
+++ src/app/authorization.service.ts
@@ -3,6 +3,17 @@ import {remote} from 'electron';
 
 declare var AuthenticationContext:any; //Make an existing javascript library available in TS... no net
 
+/*
+Notes:
+
+- Need to detect when the user closes the popup rather than this code...
+
+*/
+
+interface CallBackFunc {
+	(msg:string, token:any) : void;
+}
+
 @Injectable()
 export class AuthorizationService {
 
@@ -16,10 +27,14 @@ export class AuthorizationService {
       		clientId: '188743b4-4d95-4971-9d7b-1f2c105c9bda', //REPLACE WITH YOUR CLIENT ID
       		redirectUri: 'http://localhost', //REPLACE WITH YOUR REDIRECT URL
       		displayCall: this.electronSignIn.bind(this),
-      		callback: this.userSignedIn.bind(this),
-      		popUp: true
+      		//callback: this.userSignedIn.bind(this),
+      		popUp: false,
+      		extraQueryParameter: 'prompt=login'
 		})
+	}
 
+
+	private createAuthWindow(){
 		var webPreferences: Electron.WebPreferences = {};
 		var options: Electron.BrowserWindowOptions = {};
 		options.width = 800;
@@ -28,7 +43,10 @@ export class AuthorizationService {
 		webPreferences.nodeIntegration = false;
 		options.webPreferences = webPreferences;
 
+		console.log(options);
+
 		this.authWindow = new remote.BrowserWindow(options);
+
 
 		var handleRedirect = function(event: Electron.Event, oldURL:string, newURL:string, isMainFrame:boolean, httpResponseCode:number, requestMethod:string, referrer:string, headers:Electron.Headers){
 			this.handleCallBack(newURL);
@@ -45,13 +63,17 @@ export class AuthorizationService {
 		
 		this.authWindow.webContents.on("did-get-redirect-request", handleRedirectBound);
 		this.authWindow.webContents.on("will-navigate", handleNavigationBound);
-		
-	}
 
+	}
 
 	private handleCallBack(url:string){
 		if (url.indexOf(this.authContext.config.redirectUri) != -1) {
-			this.authContext.handleWindowCallback(url.substr(url.indexOf('#')));
+			console.log('got it');
+			//this.authContext.handleWindowCallback(url.substr(url.indexOf('#'))); //This doesn't work due to checkfor popup=true...
+			var hash = url.substr(url.indexOf('#'));
+			var requestInfo = this.authContext.getRequestInfo(hash);
+            this.authContext.saveTokenFromHash(requestInfo);
+            this.authContext.callback("", requestInfo.parameters[this.authContext.CONSTANTS.ID_TOKEN]);
 			this.authWindow.hide();
 		}
 	}
@@ -59,22 +81,23 @@ export class AuthorizationService {
 	private electronSignIn(url:string){
 		console.log(url);
 
+		this.createAuthWindow();
 		this.authWindow.loadURL(url);
 		this.authWindow.show();
 	}
 
-	private userSignedIn(msg: string, token:any){
-		console.log(msg);
-		console.log(token);
-		console.log(this.authContext.getCachedUser());
-	}
-
-	public signIn(){
+	public signIn(callback: CallBackFunc){
+		this.authContext.callback = callback;
 		this.authContext.login();
 	}
 
-	public acquireToken(resourceId: string){
+	public acquireToken(callback: CallBackFunc, resource: string){
+		this.authContext.callback = callback;
+		this.authContext.acquireToken(resource);
+	}
 
+	public getCachedUser(){
+		return this.authContext.getCachedUser();
 	}
 
 
